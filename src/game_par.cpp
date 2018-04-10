24b40c5c42a6df74636161c7d04f380b966452d3
Petr Faltýnek <pfaltynek@gmail.com>
2018-04-10 14:49:22 +0200

    added game par implementation

--- src/game_par.cpp
+++ src/game_par.cpp
@@ -1,35 +1,150 @@
 #include "game_par.h"
 
-// pars:
-//	4 x 4 : 7
-//	5 x 5 : 9
-//	6 x 6 : 11
-//	7 x 7 : 13
-//	8 x 8 : 15
-//	9 x 9 : 17
-//	10 x 10 : 19
-//	11 x 11 : 20
-//	12 x 12 : 22
-//	13 x 13 : 24
-//	14 x 14 : 26
-//	15 x 15 : 28
-//	16 x 16 : 30
-//	17 x 17 : 32
-//	18 x 18 : 34
-//	19 x 19 : 36
-//	20 x 20 : 38
-//	21 x 21 : 39
-//	22 x 22 : 41
-//	23 x 23 : 43
-//	24 x 24 : 45
-//	25 x 25 : 47
-//	26 x 26 : 49
-//	27 x 27 : 51
-//	28 x 28 : 53
-//	29 x 29 : 55
-//	30 x 30 : 57
-//	31 x 31 : 58
-//	32 x 32 : 60
-//	33 x 33 : 62
-//	34 x 34 : 64
-//	35 x 35 : 66
+int GamePar::get_min_size() {
+	return GAME_SIZE_MIN;
+};
+
+int GamePar::get_max_size() {
+	return GAME_SIZE_MAX;
+};
+
+bool GamePar::get_par_for_size(int size, int &par) {
+	bool result = true;
+
+	switch (size){
+		case 4:
+			par = 7;
+			break;
+
+		case 5:
+			par = 9;
+			break;
+
+		case 6:
+			par = 11;
+			break;
+
+		case 7:
+			par = 13;
+			break;
+
+		case 8:
+			par = 15;
+			break;
+
+		case 9:
+			par = 17;
+			break;
+
+		case 10:
+			par = 19;
+			break;
+
+		case 11:
+			par = 20;
+			break;
+
+		case 12:
+			par = 22;
+			break;
+
+		case 13:
+			par = 24;
+			break;
+
+		case 14:
+			par = 26;
+			break;
+
+		case 15:
+			par = 28;
+			break;
+
+		case 16:
+			par = 30;
+			break;
+
+		case 17:
+			par = 32;
+			break;
+
+		case 18:
+			par = 34;
+			break;
+
+		case 19:
+			par = 36;
+			break;
+
+		case 20:
+			par = 38;
+			break;
+
+		case 21:
+			par = 39;
+			break;
+
+		case 22:
+			par = 41;
+			break;
+
+		case 23:
+			par = 43;
+			break;
+
+		case 24:
+			par = 45;
+			break;
+
+		case 25:
+			par = 47;
+			break;
+
+		case 26:
+			par = 49;
+			break;
+
+		case 27:
+			par = 51;
+			break;
+
+		case 28:
+			par = 53;
+			break;
+
+		case 29:
+			par = 55;
+			break;
+
+		case 30:
+			par = 57;
+			break;
+
+		case 31:
+			par = 58;
+			break;
+
+		case 32:
+			par = 60;
+			break;
+
+		case 33:
+			par = 62;
+			break;
+
+		case 34:
+			par = 64;
+			break;
+
+		case 35:
+			par = 66;
+			break;
+
+		default:
+			result = false;
+			par = -1;
+			break;
+	}
+
+	return result;
+}
--- src/game_par.h
+++ src/game_par.h
@@ -1,14 +1,13 @@
 #ifndef GAME_PAR_H
 #define GAME_PAR_H
 
+#define GAME_SIZE_MIN 4
+#define GAME_SIZE_MAX 35
+
 class GamePar {
   public:
 	int get_min_size();
 	int get_max_size();
 	bool get_par_for_size(int size, int &par);
-
-	private:
-	  int _min, _max;
-
 };
 #endif // GAME_PAR_H
